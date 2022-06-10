364ff805ac5d03c8fda082d2e8173fd2e069e9b6
ilker.saglik <ilkersaglik.ee@gmail.com>
2022-06-10 10:09:07 +0300

    plantuml plugin added

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -120,3 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'full',
+        'extraPlugins': ','.join(
+            [
+               'plantuml',
+            ]
+        ),
+    },
+}
